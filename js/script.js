04e68ed81da1163904fc14c183542fcc2ee85134
Jean Brito <jeanbrito22@gmail.com>
2019-07-18 01:06:16 -0300

    trocando os links para http

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://pokeapi.co/api/v2/pokemon/";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 const randomPokemons = [...Array(6)].map(() =>  parseInt(Math.random() * 151));
 
@@ -25,17 +25,17 @@ jQuery(document).ready(function () {
 
     jQuery.when(
 
-        jQuery.get("http://pokeapi.co/api/v2/pokemon/" + randomPokemons[0]).done( p => pokemons[0] = p ),
+        jQuery.get(BASE_URL + randomPokemons[0]).done( p => pokemons[0] = p ),
 
-        jQuery.get("http://pokeapi.co/api/v2/pokemon/" + randomPokemons[1]).done( p => pokemons[1] = p ),
+        jQuery.get(BASE_URL + randomPokemons[1]).done( p => pokemons[1] = p ),
 
-        jQuery.get("http://pokeapi.co/api/v2/pokemon/" + randomPokemons[2]).done( p => pokemons[2] = p),
+        jQuery.get(BASE_URL + randomPokemons[2]).done( p => pokemons[2] = p),
         
-        jQuery.get("http://pokeapi.co/api/v2/pokemon/" + randomPokemons[3]).done( p => pokemons[3] = p),
+        jQuery.get(BASE_URL + randomPokemons[3]).done( p => pokemons[3] = p),
     
-        jQuery.get("http://pokeapi.co/api/v2/pokemon/" + randomPokemons[4]).done( p => pokemons[4] = p),
+        jQuery.get(BASE_URL + randomPokemons[4]).done( p => pokemons[4] = p),
         
-        jQuery.get("http://pokeapi.co/api/v2/pokemon/" + randomPokemons[5]).done( p => pokemons[5] = p)
+        jQuery.get(BASE_URL + randomPokemons[5]).done( p => pokemons[5] = p)
 
     ).then(
         () => criarLinha(pokemons),
